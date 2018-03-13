236d0877fcbbeed85b8b393ecf768f2b1f9dae14
yujen <yujen.dev@gmail.com>
2018-03-13 21:32:44 +0800

    add project

---
+++ Source/ClientServerRep/ClientServerRepGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClientServerRepGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AClientServerRepGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AClientServerRepGameMode();
+};
+
+
+
